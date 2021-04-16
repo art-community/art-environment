61b4f4674e3ba84a15c898fa9598a9beaa8ec1ad
anton.bashirov <anton.sh.local@gmail.com>
2021-04-17 00:03:20 +0300

    ART

--- plugin/src/main/kotlin/configuration/PathsConfiguration.kt
+++ plugin/src/main/kotlin/configuration/PathsConfiguration.kt
@@ -1,7 +1,7 @@
-package configuration/*
+/*
  * ART
  *
- * Copyright 2020 ART
+ * Copyright 2019-2021 ART
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,8 @@ package configuration/*
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package configuration
 
 import java.nio.file.Path
 
